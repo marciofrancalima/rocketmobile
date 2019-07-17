4bca4760c7971e360c34eb262dd544db89b7c3d8
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-17 19:11:06 -0300

    Initial config with styled-components and navigation

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import colors from './styles/colors';
+
+import './config/ReactotronConfig';
+import Routes from './routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor={colors.dark} />
+      <Routes />
+    </>
+  );
+}
