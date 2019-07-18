51496a412db74f7c35563b73660cdba7f625d385
Márcio França Lima <marciofrancalima@hotmail.com>
2019-07-17 21:02:47 -0300

    Added styling on the cart page and created navigation in the header for it

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ export default function Header({ navigation }) {
   return (
     <Container>
       <Logo />
-      <BasketContainer>
+      <BasketContainer onPress={() => navigation.navigate('Cart')}>
         <Icon name="shopping-basket" color="#FFF" size={24} />
         <ItemCount>{3}</ItemCount>
       </BasketContainer>
